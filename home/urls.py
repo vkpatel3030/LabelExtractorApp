30682eca1960e6a88165891eb2e86773ebf19fb3
vkpatel3030 <vanshupatel3030@gmail.com>
2025-07-17 23:22:12 +0530

    Code commit

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+from home.meesho import meeshoindex
+from home.amazon import amazonindex
+from home.flipkart import flipkartindex
+
+urlpatterns = [
+   path("", views.index, name='home'),
+   path("meesho", meeshoindex, name='home-meesho'),
+   path("amazon", amazonindex, name='home-amazon'),
+   path("flipkart",flipkartindex, name='home-flipkart')
+   ]
